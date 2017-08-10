ef209b0963405a23348775196193b432a938d9c5
Martyn Janes <martyn@obany.com>
2017-08-10 12:45:48 +0300

    Added coveralls support

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![Join the chat at https://gitter.im/unitejs/discuss](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/unitejs/discuss?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge) [![NPM version][npm-version-image]][npm-url] [![Build Status][travis-image]][travis-url] [![NPM downloads][npm-downloads-image]][npm-url] [![MIT License][license-image]][license-url] 
+[![Join the chat at https://gitter.im/unitejs/discuss](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/unitejs/discuss?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge) [![NPM version][npm-version-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Coveralls][coveralls-image]][coveralls-url] [![NPM downloads][npm-downloads-image]][npm-url] [![MIT License][license-image]][license-url] 
 
 # UniteJS Framework
 Some core framework classes and interfaces.
@@ -12,3 +12,6 @@ Some core framework classes and interfaces.
 
 [travis-url]: http://travis-ci.org/unitejs/framework/
 [travis-image]: http://img.shields.io/travis/unitejs/framework/master.svg?style=flat
+
+[coveralls-url]: https://coveralls.io/github/unitejs/framework
+[coveralls-image]: https://img.shields.io/coveralls/unitejs/framework.svg
--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,10 @@ const tslint = require("tslint");
 const sourcemaps = require("gulp-sourcemaps");
 const del = require("del");
 const mocha = require("gulp-mocha");
+const istanbul = require("gulp-istanbul");
 const merge = require("merge2");
+const remapIstanbul = require("remap-istanbul/lib/gulpRemapIstanbul");
+const coveralls = require("gulp-coveralls");
 const runSequence = require("run-sequence");
 
 const tsConfigFile = "./tsconfig.json";
@@ -20,6 +23,8 @@ const unitSrcFolder = `${unitFolder}src/`;
 const unitDistFolder = `${unitFolder}dist/`;
 const unitSrcGlob = `${unitSrcFolder}**/*.spec.ts`;
 const unitDistGlob = `${unitDistFolder}/**/*.spec.js`;
+const unitReportsFolder = `${unitFolder}/reports/`;
+const unitReportsFolderGlob = `${unitReportsFolder}/**/*`;
 
 gulp.task("build-clean", (cb) => {
     return del(distGlob, cb);
@@ -31,7 +36,7 @@ gulp.task("build-lint", () => {
     return gulp.src(srcGlob)
         .pipe(gulpTslint({
             "formatter": "verbose",
-            "program": program
+            program
         }))
         .pipe(gulpTslint.report())
         .on("error", () => {
@@ -54,7 +59,7 @@ gulp.task("build-transpile", () => {
     const streams = [];
 
     streams.push(tsResult.js
-        .pipe(sourcemaps.write({ "includeContent": false, "sourceRoot": "../src" }))
+        .pipe(sourcemaps.write({"includeContent": true}))
         .pipe(gulp.dest(distFolder))
         .on("end", () => {
             if (errorCount > 0) {
@@ -75,7 +80,7 @@ gulp.task("build", (cb) => {
 });
 
 gulp.task("unit-clean", (cb) => {
-    return del(unitDistGlob, cb);
+    return del([unitDistGlob, unitReportsFolderGlob], cb);
 });
 
 gulp.task("unit-lint", () => {
@@ -84,7 +89,7 @@ gulp.task("unit-lint", () => {
     return gulp.src(unitSrcGlob)
         .pipe(gulpTslint({
             "formatter": "verbose",
-            "program": program
+            program
         }))
         .pipe(gulpTslint.report())
         .on("error", () => {
@@ -105,13 +110,19 @@ gulp.task("unit-transpile", () => {
         });
 
     return tsResult.js
-        .pipe(sourcemaps.write({ "includeContent": false, "sourceRoot": "../src" }))
+        .pipe(sourcemaps.write({"includeContent": true}))
         .pipe(gulp.dest(unitDistFolder))
         .on("end", () => {
             if (errorCount > 0) {
                 process.exit();
             }
-        })
+        });
+});
+
+gulp.task("unit-pre-coverage", () => {
+    return gulp.src(`${distFolder}**/*.js`)
+        .pipe(istanbul({"includeUntested": true}))
+        .pipe(istanbul.hookRequire());
 });
 
 gulp.task("unit-runner", () => {
@@ -119,11 +130,37 @@ gulp.task("unit-runner", () => {
         .pipe(mocha({
             "reporter": "spec",
             "timeout": "360000"
+        }))
+        .pipe(istanbul.writeReports({
+            "dir": unitReportsFolder,
+            "reporters": ["json"]
+        }));
+
+});
+
+gulp.task("unit-post-coverage", () => {
+    return gulp.src(`${unitReportsFolder}coverage-final.json`)
+        .pipe(remapIstanbul({
+            "reports": {
+                "json": `${unitReportsFolder}coverage.json`,
+                "html": `${unitReportsFolder}html-report`,
+                "lcovonly": `${unitReportsFolder}lcov.info`
+            }
         }));
 });
 
 gulp.task("unit", (cb) => {
-    runSequence("unit-clean", "unit-transpile", "unit-lint", "unit-runner", cb);
+    runSequence("unit-clean",
+        "unit-transpile",
+        "unit-lint",
+        "unit-pre-coverage",
+        "unit-runner",
+        "unit-post-coverage", cb);
+});
+
+gulp.task("coveralls", () => {
+    return gulp.src(`${unitReportsFolder}lcov.info`)
+        .pipe(coveralls());
 });
 
 gulp.task("clean-all", ["build-clean", "unit-clean"]);
